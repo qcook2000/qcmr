de75b099b28dad220c3229b3b256326591e4d9a9
Christian Canonigo <28989015+cjcanonigo@users.noreply.github.com>
2018-09-06 00:32:43 -0700

    make deployable

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
-import { https } from "firebase-functions"
-import setupGraphQLServer from "./graphql/server"
+// import { https } from "firebase-functions"
+// import setupGraphQLServer from "./graphql/server"
 
-/* CF for Firebase with graphql-server-express */
-const graphQLServer = setupGraphQLServer()
+// /* CF for Firebase with graphql-server-express */
+// const graphQLServer = setupGraphQLServer()
 
-// https://us-central1-<project-name>.cloudfunctions.net/api
-export const api = https.onRequest(graphQLServer)
+// // https://us-central1-<project-name>.cloudfunctions.net/api
+// export const api = https.onRequest(graphQLServer)
